a0967a9d3f672cfa5cb538a9c9756b4097a70a8f
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-12-17 10:21:03 +0600

    defined all routes and config the firebase and tailwind css

---
+++ src/routes/routes.jsx
@@ -0,0 +1,52 @@
+import { createBrowserRouter, Navigate } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Main Layout</div>,
+    children: [
+      {
+        index: true,
+        element: <div>Home page</div>,
+      },
+      { path: "/meals", element: <div>Meals page</div> },
+      { path: "/meals/:id", element: <div>Meals details page</div> },
+      { path: "/create-order", element: <div>Create Order page</div> },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <div>Auth Layout</div>,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/auth/login" />,
+      },
+      { path: "login", element: <div>Login Page</div> },
+      { path: "register", element: <div>Register Page</div> },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <div>dashboard Layout</div>,
+    children: [
+      {
+        index: true,
+        element: <div>Dashboard</div>,
+      },
+      { path: "profile", element: <div>profile</div> },
+      { path: "orders", element: <div>orders</div> },
+      { path: "reviews", element: <div>reviews</div> },
+      { path: "favorites", element: <div>favorites</div> },
+      { path: "create-meal", element: <div>create-meal</div> },
+      { path: "my-meals", element: <div>my-meals</div> },
+      { path: "order-requests", element: <div>order-requests</div> },
+      { path: "users", element: <div>users</div> },
+      { path: "requests", element: <div>requests</div> },
+      { path: "statistics", element: <div>statistics</div> },
+    ],
+  },
+  { path: "*", element: <div>Error Page</div> },
+]);
+
+export default router;
